1260efdc2a7c119c9c2d5988d377163326ae961c
alireza <alireza.alemi76@icloud.com>
2023-02-23 13:32:03 -0700

    added levelTraversal.

---
+++ tree/binaryTreeLevelOrderTraversal.js
@@ -0,0 +1,40 @@
+// Binary Tree Level Order Traversal
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrder = function (root) {
+  const result = [];
+  const queue = [];
+  if (root) queue.push(root);
+  while (queue.length > 0) {
+    const temp = [];
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      temp.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(temp);
+  }
+  return result;
+};
